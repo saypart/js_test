54bab955fe28cf37c1764224804e4a5b4d8b44eb
unknown <saypart@gmail.com>
2023-08-10 14:49:33 +0900

    회원정보 전체 불러오기 추가

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ let idNum = 1;
 let idList = [];
 let nameList = [];
 let phoneNumList = [];
-
+let listAllsLog = [];
+let listAlls = new Object();
 
 function arrs(){
     let arr = ["가","나",3,4,"바"];
@@ -17,6 +18,32 @@ function arrs(){
         console.log(arr[i]+0);    
     }
 }
+
+function memberFindAll(){
+    for (var i = 0; i < idList.length; i++){
+        let listAll = new Object();
+        listAll ['아이디'] = idList[i];
+        listAll ['이름'] = nameList[i];
+        listAll ['전화번호'] = phoneNumList[i];    
+    
+        listAlls [i] = listAll;
+    }
+    return listAlls
+}
+
+
+
+
+
+function memberAllcount(){
+    let Allcount = idList.length;
+    // let Allcount = listAlls.length;
+    return Allcount;
+}
+
+
+
+
 
 function memberAdd(name,tel){
     idList.push(idNum);
@@ -30,6 +57,36 @@ function memberFind(){
     findName  = nameList[num-1];
     return findName;
 }
+
+
+function phoneNumFind(tel){
+    var check  = phoneNumList.indexOf(tel);
+    return check;
+}
+
+/*
+function tableCreate(){
+
+    var table_value = new Array();
+    var html = '';
+    table_value.push({value : '25.5', time : '2020-08-26'}); 
+    table_value.push({value : '22.4', time : '2020-08-26'}); 
+    table_value.push({value : '10.58', time : '2020-08-26'}); 
+    table_value.push({value : '3.44', time : '2020-08-26'}); 
+    table_value.push({value : '8.5', time : '2020-08-26'}); 
+    
+    for(i in table_value){
+    html += '<tr>';
+    html += '<td>'+table_value[i].value+'</td>';
+    html += '<td>'+table_value[i].time+'</td>';
+    html += '</tr>';
+    }
+    
+    document.getElementsById("kgTable").empty();
+    document.getElementsById("kgTable").append(html);
+    
+    }
+  */  
 
 
 function variable(){
